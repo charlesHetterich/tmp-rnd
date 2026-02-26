9d4556008156d9b201c1ff9587b130ba7a8343e2
Charles Hetterich <hetterich.charles@gmail.com>
2026-02-26 20:25:22 +0000

    removed old content

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-# Get dependencies for zombienet
-curl -sSL https://raw.githubusercontent.com/paritytech/ppn-proxy/main/install.sh | bash
-
-# Get reference repositories for Claude
-rm -rf ./references
-mkdir -p ./references
-git clone https://github.com/use-ink/ink ./references/ink
-git clone https://github.com/polkadot-api/polkadot-api.git ./references/polkadot-api
-git clone https://github.com/paritytech/cargo-pvm-contract.git ./references/cargo-pvm-contract --depth 1 --branch feat/abi-generation-and-macro-improvements
